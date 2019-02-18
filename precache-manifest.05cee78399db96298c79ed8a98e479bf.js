7c4e0566a6d65765c6f7c5871dbf44d82ef859d3
Miroslav P <miroslav.petkevic@gmail.com>
2019-02-18 15:02:59 +0200

    Updates

---
+++ precache-manifest.05cee78399db96298c79ed8a98e479bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "665939857e5f4860b6ea",
+    "url": "/nfq-barbershop/static/js/runtime~main.66593985.js"
+  },
+  {
+    "revision": "24ead68ceb71726f139e",
+    "url": "/nfq-barbershop/static/js/main.24ead68c.chunk.js"
+  },
+  {
+    "revision": "b5cbb87093393a2b9ea0",
+    "url": "/nfq-barbershop/static/js/2.b5cbb870.chunk.js"
+  },
+  {
+    "revision": "24ead68ceb71726f139e",
+    "url": "/nfq-barbershop/static/css/main.3a8ee42b.chunk.css"
+  },
+  {
+    "revision": "b5cbb87093393a2b9ea0",
+    "url": "/nfq-barbershop/static/css/2.c3448836.chunk.css"
+  },
+  {
+    "revision": "8311a8897cec38bf567db273707906e3",
+    "url": "/nfq-barbershop/index.html"
+  }
+];
